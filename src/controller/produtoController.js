e9ffa7054480c235d4634f261b3bb22bad58c71d
william <williamramos4994@gmail.com>
2023-09-19 16:20:41 -0300

    cadastro ok

--- src/controller/produtoController.js
+++ src/controller/produtoController.js
@@ -4,8 +4,49 @@ import { teste, cadastrarDetalhes, cadastrarProduto, cadastrarImagens } from '..
 
 const produtoEndpoints = Router();
 
-produtoEndpoints.post('/detalhes', async (req, resp) => {
-    const resposta = {
+// produtoEndpoints.post('/detalhes', async (req, resp) => {
+//     const resposta = {
+//         intensidade: req.body.intensidade,
+//         docura: req.body.docura,
+//         acidez: req.body.acidez,
+//         torra: req.body.torra,
+//         descricao: req.body.descricao,
+//         marca: req.body.marca,
+//         peso: req.body.peso,
+//         alergia: req.body.alergia,
+//         dimensoes: req.body.dimensoes
+//     };
+
+//     const idDetalhe = await cadastrarDetalhes(resposta);
+//     const json = {
+//         id: idDetalhe
+//     }
+
+//     resp.json(json);
+// })
+
+// produtoEndpoints.post('/produto', async (req, resp) => {
+//     const info = {
+//             idAdm: req.body.idAdm,
+//             idCategoria: req.body.idCategoria,
+//             nome: req.body.nome,
+//             preco: req.body.preco,
+//             promocional: req.body.promocional,
+//             disponivelAssinatura: req.body.disponivelAssinatura,
+//             estoque: req.body.estoque
+//         };
+        
+//     const cadastro = await cadastrarProduto(info);
+//     resp.send(cadastro);
+// })
+
+produtoEndpoints.post('/imagem', async (req, resp) => {
+    
+})
+
+
+produtoEndpoints.post('/produto', async (req, resp) => {
+    const infoDetalhes = {
         intensidade: req.body.intensidade,
         docura: req.body.docura,
         acidez: req.body.acidez,
@@ -17,31 +58,22 @@ produtoEndpoints.post('/detalhes', async (req, resp) => {
         dimensoes: req.body.dimensoes
     };
 
-    const idDetalhe = await cadastrarDetalhes(resposta);
-    const json = {
-        id: idDetalhe
-    }
-
-    resp.json(json);
-})
-
-produtoEndpoints.post('/produto', async (req, resp) => {
-    const info = {
+    const idDetalhe = await cadastrarDetalhes(infoDetalhes);
+    
+    const infoProduto = {
+            idDetalhe: idDetalhe,
             idAdm: req.body.idAdm,
             idCategoria: req.body.idCategoria,
             nome: req.body.nome,
             preco: req.body.preco,
             promocional: req.body.promocional,
             disponivelAssinatura: req.body.disponivelAssinatura,
-            estoque: req.body.estoque,
+            estoque: req.body.estoque
         };
         
-    const cadastro = await cadastrarProduto(info);
+    const cadastro = await cadastrarProduto(infoProduto);
     resp.send(cadastro);
-})
-
-produtoEndpoints.post('/imagem', async (req, resp) => {
-    
-})
+});
 
 export default produtoEndpoints;
+
