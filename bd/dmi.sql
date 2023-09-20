072c7188adb4f723c4db8290f1835c3082de2bb1
Pietro <pietroaug806@gmail.com>
2023-09-19 22:56:23 -0300

    alteração no dmi

--- bd/dmi.sql
+++ bd/dmi.sql
@@ -11,3 +11,14 @@ and    ds_senha = ?
 -- inserindo cliente
 insert into tb_cliente(nm_cliente, ds_telefone, ds_cpf, dt_nascimento, ds_email, ds_senha, dt_cadastro)
                                 values(?, ?, ?, ?, ?, ?, now());
+
+
+-- Inserindo Adm
+insert into tb_admin(ds_email, nm_usuario, ds_senha, img_adm)
+				values('pietro@gmail.com', 'pietro', '1234', null);
+insert into tb_admin(ds_email, nm_usuario, ds_senha, img_adm)
+				values('william@gmail.com', 'william', '1234', null);
+insert into tb_admin(ds_email, nm_usuario, ds_senha, img_adm)
+				values('thiago@gmail.com', 'thiago', '1234', null);
+insert into tb_admin(ds_email, nm_usuario, ds_senha, img_adm)
+				values('eduardo@gmail.com', 'eduardo', '1234', null);
