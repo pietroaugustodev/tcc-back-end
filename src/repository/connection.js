6f2c80366ae96989619b0961bf1fa040593da7b6
Pietro <pietroaug806@gmail.com>
2023-10-27 21:52:19 -0300

    alterações para usar o putty

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise'
 
 const conexao = await mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
 });
 
 
